d1724509ac4708cbc55b198998badaff09156950
Kirill Ishtuganov <ian574miller@gmail.com>
2024-07-15 16:44:22 +0300

    make new courses pages

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,22 +1,41 @@
 package org.example.hexlet;
 
 import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+import org.example.hexlet.dto.courses.CoursePage;
+import org.example.hexlet.dto.courses.CoursesPage;
+
+import static io.javalin.rendering.template.TemplateUtil.model;
+import static org.example.hexlet.Data.getCourse;
+import static org.example.hexlet.Data.getCourses;
 
 public class HelloWorld {
     public static void main(String[] args) {
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
         });
+        app.get("/", ctx -> ctx.render("index.jte"));
         app.get("/users", ctx -> ctx.result("GET /users"));
         app.post("/users", ctx -> ctx.result("POST /users"));
+
         app.get("/hello", ctx -> {
             var name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
             ctx.result("Hello, " + name + "!");
         });
-        app.get("/courses/{courseId}/lessons/{id}", ctx -> {
-            var courseId = ctx.pathParam("courseId");
-            var lessonId =  ctx.pathParam("id");
-            ctx.result("Course ID: " + courseId + " Lesson ID: " + lessonId);
+
+        app.get("/courses", ctx -> {
+            var courses = getCourses();
+            var header = "Курсы по программированию";
+            var page = new CoursesPage(courses, header);
+            ctx.render("courses/index.jte", model("page", page));
+        });
+
+        app.get("/courses/{id}", ctx -> {
+            var id = ctx.pathParam("id");
+            var course = getCourse(Integer.parseInt(id));
+            var page = new CoursePage(course);
+            ctx.render("courses/show.jte", model("page", page));
         });
         app.start(7070);
     }
